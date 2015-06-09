5dcdac905f354dd963eade035273436ade8d4a1d
dis-zephyro <zephyro@yandex.ru>
2015-06-09 15:02:49 +0300

    ssss

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// подсказки для форм (можноу удалить)
+
+$(function(){
+    var mr_input="input[type=text]";
+    $(mr_input).each(function() {    $(this).data('stext',$(this).val());                                               });
+    $(mr_input).focus(function(){	 $(this).val()==$(this).data('stext')  ?   $(this).val(''): "0";	               	});
+    $(mr_input).blur(function() {    $(this).val().length<=0               ?   $(this).val($(this).data('stext')): "0"; });
+});
+
 
 $(document).ready(function() {
 
